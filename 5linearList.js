e38d240a26651c7dda65c1367141cf3170e78ead
JorgeMioska <quispechilojorge@gmail.com>
2023-01-10 19:10:54 -0500

    print solved + addLast

--- 5linearList.js
+++ 5linearList.js
@@ -1,38 +1,54 @@
-class Node{
-    constructor(value){
-        this.value=value;
-        this.pointer=null;
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.pointer = null;
+  }
 }
-class linkedList{
-    constructor(){
-        this.head=null;
-        this.length=0;
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+  size() {
+    return this.length;
+  }
+  start() {
+    if (this.head === null) {
+      return null;
+    } else {
+      return this.head;
     }
-    size(){
-        return this.length;
+  }
+  isEmpty() {
+    return this.length === 0;
+  }
+  addLast(val) {
+    if (this.head === null) {
+      this.head = new Node(val);
+      this.length++;
+      return;
     }
-    start(){
-        if(this.head===null){
-            return null
-        }else{
-            return this.head
-        }
+    let currentNode = this.head;
+    while(currentNode.pointer){
+        currentNode=currentNode.pointer
     }
-    add(val){
-        if(this.head===null){
-            this.head=val;
-        }
-        this.length++;
+    const node = new Node(val);
+    currentNode.pointer = node;
+    this.length++;
+  }
+  print() {
+    let values=0;
+    let currentNode=this.head;
+    for (let i = 0; i < this.length; i++) {
+      currentNode=currentNode.pointer;
+      values++;
     }
-    print(){
-        for (let i=0;i<this.length;i++){
-            return this.head;
-        }
-    }
+    return values
+  }
 }
-newList=new linkedList();
-newList.add(87);
-console.log(newList.print())
-console.log(newList.length)
-console.log(newList.start())
+newList = new linkedList();
+newList.addLast(1);
+newList.addLast(2);
+newList.addLast(4);
+console.log(newList.print());
+
